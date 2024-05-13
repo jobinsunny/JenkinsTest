e646c4414ed5da2aa5069353246f33c78e62f147
Jobin Sunny <66160325+jobinsunny@users.noreply.github.com>
2024-05-13 16:31:21 +0530

    Update entrypoint.sh

--- resources/entrypoint.sh
+++ resources/entrypoint.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-context="gitlab"
-gitlab_protocol="ssh"
-gitlab_protocol_2="http"
-host=$GITLAB_HOST
-port=$GITLAB_PORT
-gitlab_provider_id="adop-gitlab"
+# context="gitlab"
+# gitlab_protocol="ssh"
+# gitlab_protocol_2="http"
+# host=$GITLAB_HOST
+# port=$GITLAB_PORT
+# gitlab_provider_id="adop-gitlab"
 
-echo "Genarate JENKINS SSH KEY"
-nohup /usr/share/jenkins/ref/adop\_scripts/generate_key.sh -c ${host} -p ${port} &
+# echo "Genarate JENKINS SSH KEY"
+# nohup /usr/share/jenkins/ref/adop\_scripts/generate_key.sh -c ${host} -p ${port} &
 
-echo "Setting up your default SCM provider - Gitlab..."
-mkdir -p $PLUGGABLE_SCM_PROVIDER_PROPERTIES_PATH $PLUGGABLE_SCM_PROVIDER_PATH
-mkdir -p ${PLUGGABLE_SCM_PROVIDER_PROPERTIES_PATH}/CartridgeLoader ${PLUGGABLE_SCM_PROVIDER_PROPERTIES_PATH}/ScmProviders
-nohup /usr/share/jenkins/ref/adop\_scripts/generate_gitlab_scm.sh -i ${gitlab_provider_id} -p ${gitlab_protocol} -p ${gitlab_protocol_2} -h ${host} -c ${context} &
+# echo "Setting up your default SCM provider - Gitlab..."
+# mkdir -p $PLUGGABLE_SCM_PROVIDER_PROPERTIES_PATH $PLUGGABLE_SCM_PROVIDER_PATH
+# mkdir -p ${PLUGGABLE_SCM_PROVIDER_PROPERTIES_PATH}/CartridgeLoader ${PLUGGABLE_SCM_PROVIDER_PROPERTIES_PATH}/ScmProviders
+# nohup /usr/share/jenkins/ref/adop\_scripts/generate_gitlab_scm.sh -i ${gitlab_provider_id} -p ${gitlab_protocol} -p ${gitlab_protocol_2} -h ${host} -c ${context} &
 
-echo "Generate Sonar authentication token"
-source /usr/share/jenkins/ref/adop\_scripts/generate_sonar_auth_token.sh
+# echo "Generate Sonar authentication token"
+# source /usr/share/jenkins/ref/adop\_scripts/generate_sonar_auth_token.sh
 
-echo "Generate GitLab authentication token"
-. /usr/share/jenkins/ref/adop\_scripts/generate_gitlab_auth_token.sh
+# echo "Generate GitLab authentication token"
+# . /usr/share/jenkins/ref/adop\_scripts/generate_gitlab_auth_token.sh
 
-echo "Copy Jenkins's public key to GitLab"
-source /usr/share/jenkins/ref/adop\_scripts/put_jenkins_public_key_in_gitlab.sh
+# echo "Copy Jenkins's public key to GitLab"
+# source /usr/share/jenkins/ref/adop\_scripts/put_jenkins_public_key_in_gitlab.sh
 
-echo "skip upgrade wizard step after installation"
-echo "2.7.4" > /var/jenkins_home/jenkins.install.UpgradeWizard.state
+# echo "skip upgrade wizard step after installation"
+# echo "2.7.4" > /var/jenkins_home/jenkins.install.UpgradeWizard.state
 
 echo "start JENKINS"
 
